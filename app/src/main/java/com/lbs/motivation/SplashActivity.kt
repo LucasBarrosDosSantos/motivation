13e87087ab056dc929b28f7251dd7b9bd74d2077
Lucas Barros <lucas2019.fullstack@gmail.com>
2020-07-29 23:49:17 -0300

    feature: Validate data and go to other activity

--- app/src/main/java/com/lbs/motivation/SplashActivity.kt
+++ app/src/main/java/com/lbs/motivation/SplashActivity.kt
@@ -1,9 +1,13 @@
 package com.lbs.motivation
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_splash.*
 
-class SplashActivity : AppCompatActivity() {
+class SplashActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
@@ -11,6 +15,27 @@ class SplashActivity : AppCompatActivity() {
 
         if (supportActionBar != null) {
             supportActionBar!!.hide();
+        }
+
+        save.setOnClickListener(this)
+    }
+
+    override fun onClick(view: View) {
+        val id = view.id;
+        if (id == R.id.save) {
+            handleSave()
+        }
+    }
+
+
+    private fun handleSave() {
+        val name = editName.text.toString();
+
+        if (name != "") {
+//            use reflection for reference activity
+            startActivity(Intent(this, MainActivity::class.java))
+        } else {
+            Toast.makeText(this, getString(R.string.please_enter_name), Toast.LENGTH_LONG).show()
         }
     }
 }
