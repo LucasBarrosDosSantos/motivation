91d9600bd16f7312028aa8cf383abbfa13342699
Lucas Barros <lucas2019.fullstack@gmail.com>
2020-07-31 20:21:22 -0300

    feature: Add tree images and event click on style

--- app/src/main/java/com/lbs/motivation/ui/MainActivity.kt
+++ app/src/main/java/com/lbs/motivation/ui/MainActivity.kt
@@ -2,11 +2,14 @@ package com.lbs.motivation.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.core.content.ContextCompat
 import com.lbs.motivation.R
 import com.lbs.motivation.infra.MotivationConstants
 import com.lbs.motivation.infra.SecurityPreferences
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     private lateinit var securityPreferences: SecurityPreferences
 
@@ -19,6 +22,51 @@ class MainActivity : AppCompatActivity() {
 
         securityPreferences.getString(MotivationConstants.KEY.PERSON_NAME)
 
+        newPhrase.setOnClickListener(this)
+        allImages.setOnClickListener(this)
+        imageHappy.setOnClickListener(this)
+        imageMorning.setOnClickListener(this)
+
+        if (supportActionBar != null) {
+            supportActionBar!!.hide()
+        }
+
+
+    }
+
+    override fun onClick(view: View) {
+        val id = view.id;
+
+        val listFilter = listOf(R.id.allImages, R.id.imageHappy, R.id.imageMorning)
+
+        if (id == R.id.newPhrase) {
+            handleNewPhrase()
+        } else if (id in listFilter) {
+            handleFilter(id)
+        }
+    }
+
+    private fun handleFilter(id: Int) {
+
+        allImages.setColorFilter(ContextCompat.getColor(this, R.color.white))
+        imageHappy.setColorFilter(ContextCompat.getColor(this, R.color.white))
+        imageMorning.setColorFilter(ContextCompat.getColor(this, R.color.white))
+        when (id) {
+            R.id.allImages -> {
+                allImages.setColorFilter(ContextCompat.getColor(this, R.color.colorAccent))
+            }
+            R.id.imageHappy -> {
+                imageHappy.setColorFilter(ContextCompat.getColor(this, R.color.colorAccent))
+
+            }
+            R.id.imageMorning -> {
+                imageMorning.setColorFilter(ContextCompat.getColor(this, R.color.colorAccent))
+
+            }
+        }
+    }
+
+    private fun handleNewPhrase() {
 
     }
 }
