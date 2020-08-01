49c3d269a7eceb6c8271681705fbff52c8ce8c12
Lucas Barros <lucas2019.fullstack@gmail.com>
2020-07-31 21:52:00 -0300

    refactory interface for connect data repository

--- app/src/main/java/com/lbs/motivation/ui/MainActivity.kt
+++ app/src/main/java/com/lbs/motivation/ui/MainActivity.kt
@@ -7,29 +7,35 @@ import androidx.core.content.ContextCompat
 import com.lbs.motivation.R
 import com.lbs.motivation.infra.MotivationConstants
 import com.lbs.motivation.infra.SecurityPreferences
+import com.lbs.motivation.repository.Mock
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     private lateinit var securityPreferences: SecurityPreferences
+    private var phraseFilter: Int = MotivationConstants.PHRASEFILTER.ALL
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        securityPreferences = SecurityPreferences(this)
+        if (supportActionBar != null) {
+            supportActionBar!!.hide()
+        }
 
+        securityPreferences = SecurityPreferences(this)
         securityPreferences.getString(MotivationConstants.KEY.PERSON_NAME)
+
+
+//        init screen with value on icon
+        allImages.setColorFilter(ContextCompat.getColor(this, R.color.colorAccent))
+        handleNewPhrase()
 
         newPhrase.setOnClickListener(this)
         allImages.setOnClickListener(this)
         imageHappy.setOnClickListener(this)
         imageMorning.setOnClickListener(this)
-
-        if (supportActionBar != null) {
-            supportActionBar!!.hide()
-        }
 
 
     }
@@ -54,19 +60,20 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         when (id) {
             R.id.allImages -> {
                 allImages.setColorFilter(ContextCompat.getColor(this, R.color.colorAccent))
+                phraseFilter = MotivationConstants.PHRASEFILTER.ALL
             }
             R.id.imageHappy -> {
                 imageHappy.setColorFilter(ContextCompat.getColor(this, R.color.colorAccent))
-
+                phraseFilter = MotivationConstants.PHRASEFILTER.HAPPY
             }
             R.id.imageMorning -> {
                 imageMorning.setColorFilter(ContextCompat.getColor(this, R.color.colorAccent))
-
+                phraseFilter = MotivationConstants.PHRASEFILTER.MORNING
             }
         }
     }
 
     private fun handleNewPhrase() {
-
+        textPhrase.text = Mock().getPhrase(phraseFilter)
     }
 }
